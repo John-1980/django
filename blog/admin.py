83cb54c7318746c29d61d60e36f9e09274ba1f51
Rogerio Teixeira <rrssouzza@hotmail.com>
2021-01-16 21:25:37 -0300

    Managers e Querysets

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish', 'status')
+    list_filter = ('title', 'author', 'publish', 'status')
+    search_fields = ('title', 'content')
+    date_hierarchy = 'publish'
+
+    # pesquisa
+    prepopulated_fields = {'slug': ('title',)}
